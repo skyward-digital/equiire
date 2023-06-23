33ca432f4d91733f264aeb9b02f705482f6450dc
Dan Spratling <dan@skyward.digital>
2023-06-23 19:32:25 +0100

    Fixes storybook font issues
    Moved the fonts to the public folder so they're rendered statically, then updated the imports to match

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,12 +1,6 @@
 import { cache } from 'react';
 
 export const getBaseUrl = cache(() => {
-  console.log(
-    process.env.VERCEL_URL
-      ? `https://app-dir.vercel.app`
-      : `http://localhost:${process.env.PORT ?? 3000}`,
-  );
-
   return process.env.VERCEL_URL
     ? `https://app-dir.vercel.app`
     : `http://localhost:${process.env.PORT ?? 3000}`;
