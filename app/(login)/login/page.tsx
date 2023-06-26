f180c0430b262ea29ba41cadcd3dd56f15c2df84
Lucy Macartney <macartney.lucy@gmail.com>
2023-06-26 20:14:46 +0100

    Changes mazzard from font-sans to font-brand

--- app/(login)/login/page.tsx
+++ app/(login)/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from '#/ui/components/LoginForm';
 export default function Page() {
   return (
     <div className="prose prose-sm dark:prose-invert max-w-none">
-      <h1 className="font-sans text-xl font-bold">Login</h1>
+      <h1 className="font-brand text-xl font-bold">Login</h1>
 
       <LoginForm />
     </div>
