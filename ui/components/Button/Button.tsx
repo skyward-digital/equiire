b8279fe95386136ca19f92e480adba273c9ed89b
Lucy Macartney <macartney.lucy@gmail.com>
2023-06-26 11:51:04 +0100

    Changes border radius and improves button props

--- ui/components/Button/Button.tsx
+++ ui/components/Button/Button.tsx
@@ -22,7 +22,7 @@ const classes = {
   generic:
     'inline-flex items-center justify-center gap-2 px-6 py-3 font-sans text-base font-semibold no-underline outline-none transition duration-200 ease-in-out focus:ring-2 focus:ring-gray-600 focus:ring-offset-2',
   primary:
-    'bg-brand-primary shadow-xs hover:bg-brand-secondary rounded-lg text-white dark:text-black',
+    'bg-brand-primary shadow-xs hover:bg-brand-secondary rounded-full text-white dark:text-black',
   link: 'text-brand-primary hover:text-gray-500',
 };
 
@@ -30,18 +30,18 @@ export const Button = ({
   style,
   arrow,
   className,
-  type,
   children,
-}: ButtonProps) => {
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement> & ButtonProps) => {
   return (
     <button
-      type={type}
       className={clsx(
         className,
         classes.generic,
         style === 'primary' && classes.primary,
         style === 'link' && classes.link,
       )}
+      {...props}
     >
       {children}
       {arrow ? <ArrowRightIcon className="h-5 w-5" /> : null}
