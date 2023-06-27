eb9253c997505b937ce94e971404e3f678418552
Dan Spratling <dan@skyward.digital>
2023-06-27 12:30:10 +0100

    Allows settings cards to be toggleable

--- ui/components/SettingsCard/SettingsCard.tsx
+++ ui/components/SettingsCard/SettingsCard.tsx
@@ -17,12 +17,17 @@ export const SettingsCard = ({
   Icon,
   children,
 }: SettingsCardProps) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+
+  const handleSave = () => {
+    console.log('save');
+    setExpanded(false);
+  };
 
   return (
     <div className="dark:border-brand-secondary rounded-xl border border-gray-300 bg-white px-4 py-2 dark:bg-black">
       {/* Title area */}
-      <div className="flex items-center justify-between gap-4">
+      <div className="flex h-16 items-center justify-between gap-4">
         <div className="flex items-center gap-4 text-gray-600 dark:text-gray-300">
           <Icon className="h-6 w-6" />
           <div className="not-prose flex flex-col">
@@ -34,28 +39,51 @@ export const SettingsCard = ({
             )}
           </div>
         </div>
-        <div className="flex gap-4">
-          {!detail && (
-            <p className="flex items-center gap-1 rounded-lg border border-orange-200 bg-orange-50 px-2 py-0.5 ">
-              <ExclamationCircleIcon
-                className="h-5 w-5 text-orange-600"
-                strokeWidth={1.5}
-              />
-              <span className="font-semibold text-orange-700">
-                Missing Info
-              </span>
-            </p>
-          )}
-          <button className="text-sm text-gray-600 hover:text-gray-800">
-            Edit
-          </button>
-        </div>
+
+        {expanded ? (
+          <div className="flex gap-4">
+            <button
+              className="text-sm text-gray-600 hover:text-gray-800"
+              onClick={() => setExpanded(false)}
+            >
+              Cancel
+            </button>
+            <button
+              className="text-sm text-gray-600 hover:text-gray-800"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </div>
+        ) : (
+          <div className="flex gap-4">
+            {!detail && (
+              <p className="flex items-center gap-1 rounded-lg border border-orange-200 bg-orange-50 px-2 py-0.5 ">
+                <ExclamationCircleIcon
+                  className="h-5 w-5 text-orange-600"
+                  strokeWidth={1.5}
+                />
+                <span className="font-semibold text-orange-700">
+                  Missing Info
+                </span>
+              </p>
+            )}
+            <button
+              className="text-sm text-gray-600 hover:text-gray-800"
+              onClick={() => setExpanded(true)}
+            >
+              Edit
+            </button>
+          </div>
+        )}
       </div>
 
       {/* Editable area */}
-      <div className="mb-8 mt-4 grid gap-2 border-t border-gray-200 px-10 pt-6 text-sm">
-        {children}
-      </div>
+      {expanded && (
+        <div className="mb-8 mt-4 grid gap-2 border-t border-gray-200 px-10 pt-6 text-sm">
+          {children}
+        </div>
+      )}
     </div>
   );
 };
