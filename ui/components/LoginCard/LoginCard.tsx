48f7ddc7d018fee5dcec678164535b0e9124dc54
Lucy Macartney <macartney.lucy@gmail.com>
2023-07-07 12:51:21 +0100

    Makes title look better on mobile designs

--- ui/components/LoginCard/LoginCard.tsx
+++ ui/components/LoginCard/LoginCard.tsx
@@ -6,7 +6,7 @@ export function LoginCard({ className }: { className: string }) {
     <section
       className={`${className} shadow-xs mx-auto max-w-[557px] rounded-lg border border-gray-100 bg-white px-1 pb-16 pt-20 dark:border-gray-600 dark:bg-black sm:px-24`}
     >
-      <h1 className="text-brand-primary font-brand mb-10 text-3xl font-bold">
+      <h1 className="text-brand-primary font-brand mb-10 text-center text-3xl font-bold sm:text-left">
         Smart and Flexible <span className="text-brand-secondary">Loans</span>
       </h1>
       <LoginForm className="px-3" />
