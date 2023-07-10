9e7fb937953023ef890c857cbfbe64cf89948c0d
Lucy Macartney <macartney.lucy@gmail.com>
2023-07-10 16:11:07 +0100

    Code review clsx changes

--- ui/components/LoginCard/LoginCard.tsx
+++ ui/components/LoginCard/LoginCard.tsx
@@ -1,10 +1,14 @@
-import { LoginForm } from '../LoginForm';
+import clsx from 'clsx';
+import { LoginForm } from '#/ui/components/LoginForm';
 import { Logo } from '#/ui/assets/Logo';
 
 export function LoginCard({ className }: { className: string }) {
   return (
     <section
-      className={`${className} shadow-xs mx-auto max-w-[557px] rounded-lg border border-gray-100 bg-white px-1 pb-16 pt-20 dark:border-gray-600 dark:bg-black sm:px-24`}
+      className={clsx(
+        className,
+        'shadow-xs mx-auto max-w-[557px] rounded-lg border border-gray-100 bg-white px-1 pb-16 pt-20 dark:border-gray-600 dark:bg-black sm:px-24',
+      )}
     >
       <h1 className="text-brand-primary font-brand mb-10 text-center text-3xl font-bold sm:text-left">
         Smart and Flexible <span className="text-brand-secondary">Loans</span>
