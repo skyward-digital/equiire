50ba22c4f01be1e61831832c70223cdf472fd29d
Dan Spratling <dan@skyward.digital>
2023-07-13 17:34:35 +0100

    Notification banner dark mode

--- ui/components/NotificationBanner/NotificationBanner.tsx
+++ ui/components/NotificationBanner/NotificationBanner.tsx
@@ -25,24 +25,28 @@ export const NotificationBanner = ({
   const Icon = mapIcon[status];
 
   return (
-    <div className="flex w-full rounded-lg border bg-white p-4 shadow-sm">
+    <div className="flex w-full rounded-lg border bg-white p-4 shadow-sm dark:border-gray-500 dark:bg-black">
       <div
         className={clsx(
           'mr-4 flex items-center justify-center rounded-full border-8 p-2',
           {
-            'border-success-50 bg-success-100': status === 'success',
-            'border-warning-50 bg-warning-100': status === 'warning',
-            'border-error-50 bg-error-100': status === 'error',
-            'border-blue-50 bg-blue-100': status === 'info',
+            'border-success-50 bg-success-100 dark:border-success-700 dark:bg-success-600':
+              status === 'success',
+            'border-warning-50 dark:border-warning-700 bg-warning-100 dark:bg-warning-600':
+              status === 'warning',
+            'border-error-50 bg-error-100 dark:border-error-700 dark:bg-error-600':
+              status === 'error',
+            'border-blue-50 bg-blue-100 dark:border-blue-700 dark:bg-blue-600':
+              status === 'info',
           },
         )}
       >
         <Icon
           className={clsx('h-8 w-8', {
-            'text-success-600': status === 'success',
-            'text-warning-600': status === 'warning',
-            'text-error-600': status === 'error',
-            'text-blue-600': status === 'info',
+            'text-success-600 dark:text-success-100': status === 'success',
+            'text-warning-600 dark:text-warning-100': status === 'warning',
+            'text-error-600 dark:text-error-100': status === 'error',
+            'text-blue-600 dark:text-blue-100': status === 'info',
           })}
         />
       </div>
