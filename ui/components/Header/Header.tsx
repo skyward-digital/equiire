d2e6e25954ff780d3a3eddee10e0c12e423271d8
Dan Spratling <dan@skyward.digital>
2023-07-14 07:48:10 +0100

    Updates colours to better match designs (#12)

--- ui/components/Header/Header.tsx
+++ ui/components/Header/Header.tsx
@@ -9,10 +9,10 @@ export type HeaderProps = {
 
 export const Header = ({ title }: HeaderProps) => {
   return (
-    <header className="bg-brand-primary border-brand-primary flex items-center justify-between border-b px-6 py-2 dark:border-gray-600 dark:bg-black">
+    <header className="bg-brand border-brand flex items-center justify-between border-b px-6 py-2 dark:border-gray-600 dark:bg-black">
       <div className="flex h-14 items-center px-4 py-4 lg:h-auto">
         <Link href="/" className="group flex w-full items-center gap-x-2.5">
-          <Logo className="dark:text-brand-primary fill-current text-white" />
+          <Logo className="dark:text-brand fill-current text-white" />
           <p className="sr-only">Equiire</p>
         </Link>
       </div>
