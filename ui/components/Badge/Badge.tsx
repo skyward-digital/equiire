1d994f5d48b666328c993fe82538c78ed6a77458
Dan Spratling <dan@skyward.digital>
2023-07-17 19:02:38 +0100

    Updated badge styles (#15)

--- ui/components/Badge/Badge.tsx
+++ ui/components/Badge/Badge.tsx
@@ -2,26 +2,35 @@ import clsx from 'clsx';
 
 export type BadgeProps = {
   as?: React.ElementType;
-  type: 'info' | 'warning' | 'error';
-  Icon: any;
+  type: 'info' | 'warning' | 'error' | 'success';
+  Icon?: any;
+  dot?: boolean;
   children: React.ReactNode;
 };
 
-export const Badge = ({ as, type, Icon, children }: BadgeProps) => {
+export const Badge = ({ as, type, Icon, dot, children }: BadgeProps) => {
   const Component = as || 'p';
 
   return (
     <Component
       className={clsx(
-        'inline-flex items-center gap-1 rounded-lg border px-2 py-0.5',
+        'inline-flex items-center gap-1 rounded border px-2 py-0.5',
         {
-          'border-blue-200 bg-blue-50 text-blue-700': type === 'info',
-          'border-yellow-200 bg-yellow-50 text-yellow-700': type === 'warning',
-          'border-red-200 bg-red-50 text-red-700': type === 'error',
+          'border-blue-200 bg-blue-50 text-blue-700 dark:border-blue-900 dark:bg-blue-800 dark:text-blue-200':
+            type === 'info',
+          'border-warning-400 bg-warning-50 text-warning-700 dark:border-warning-500 dark:bg-warning-500 dark:text-warning-950':
+            type === 'warning',
+          'border-error-200 bg-error-50 text-error-700 dark:border-error-900 dark:bg-error-800 dark:text-error-200':
+            type === 'error',
+          'border-success-200 bg-success-50 text-success-700 dark:border-success-900 dark:bg-success-800 dark:text-success-200':
+            type === 'success',
         },
       )}
     >
-      <Icon className="h-5 w-5 opacity-90" strokeWidth={1.5} />
+      {Icon && <Icon className="h-5 w-5 opacity-90" strokeWidth={1.5} />}
+      {!Icon && dot ? (
+        <span className="mr-0.5 h-1.5 w-1.5 rounded-full bg-current" />
+      ) : null}
       <span className="font-semibold">{children}</span>
     </Component>
   );
