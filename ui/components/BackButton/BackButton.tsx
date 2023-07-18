7a42c429da6549f08d5b364ef8206ac93f605c8a
Lucy Macartney <macartney.lucy@gmail.com>
2023-07-18 11:47:33 +0100

    Removes redundant arrow icon

--- ui/components/BackButton/BackButton.tsx
+++ ui/components/BackButton/BackButton.tsx
@@ -11,17 +11,13 @@ export const BackButton = ({ back, ariaLabel = 'Back' }: BackButtonProps) => {
   const Component: ElementType = typeof back === 'string' ? Link : 'button';
   const props = typeof back === 'string' ? { href: back } : { onClick: back };
 
-  const ArrowIcon = () => (
-    <ArrowLeftIcon className="h-5 w-5 text-gray-400 hover:text-gray-500 dark:text-gray-300 dark:hover:text-gray-100" />
-  );
-
   return (
     <Component
       {...props}
       aria-label={ariaLabel}
       className="flex w-min items-center justify-center rounded-full bg-gray-100 p-2 dark:bg-gray-800 dark:hover:bg-gray-500"
     >
-      <ArrowIcon />
+      <ArrowLeftIcon className="h-5 w-5 text-gray-400 hover:text-gray-500 dark:text-gray-300 dark:hover:text-gray-100" />
     </Component>
   );
 };
