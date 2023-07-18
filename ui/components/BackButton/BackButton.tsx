01110d35fdfc380ef9cce9feed0bf3f348c26e60
Lucy Macartney <macartney.lucy@gmail.com>
2023-07-18 11:45:21 +0100

    Reworking back button

--- ui/components/BackButton/BackButton.tsx
+++ ui/components/BackButton/BackButton.tsx
@@ -1,3 +1,4 @@
+import { ElementType } from 'react';
 import Link from 'next/link';
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 
@@ -7,29 +8,20 @@ type BackButtonProps = {
 };
 
 export const BackButton = ({ back, ariaLabel = 'Back' }: BackButtonProps) => {
+  const Component: ElementType = typeof back === 'string' ? Link : 'button';
+  const props = typeof back === 'string' ? { href: back } : { onClick: back };
+
   const ArrowIcon = () => (
     <ArrowLeftIcon className="h-5 w-5 text-gray-400 hover:text-gray-500 dark:text-gray-300 dark:hover:text-gray-100" />
   );
 
-  if (typeof back === 'function') {
-    return (
-      <button
-        onClick={back}
-        aria-label={ariaLabel}
-        className="flex w-min items-center justify-center rounded-full bg-gray-100 p-2 dark:bg-gray-800 dark:hover:bg-gray-500"
-      >
-        <ArrowIcon />
-      </button>
-    );
-  }
-
   return (
-    <Link
-      href={back}
+    <Component
+      {...props}
       aria-label={ariaLabel}
       className="flex w-min items-center justify-center rounded-full bg-gray-100 p-2 dark:bg-gray-800 dark:hover:bg-gray-500"
     >
       <ArrowIcon />
-    </Link>
+    </Component>
   );
 };
