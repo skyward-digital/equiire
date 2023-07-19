bd0ee8e3fb880e6a670aedbf64e132669b33df39
Dan Spratling <dan@skyward.digital>
2023-07-19 10:19:02 +0100

    More cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -93,11 +93,6 @@ export default {
           950: '#053321',
         },
       },
-      backgroundImage: ({ theme }) => ({
-        'vc-border-gradient': `radial-gradient(at left top, ${theme(
-          'colors.gray.500',
-        )}, 50px, ${theme('colors.gray.800')} 50%)`,
-      }),
       boxShadow: {
         xs: '0px 1px 2px 0px rgba(16, 24, 40, 0.05)',
         sm: '0px 1px 2px 0px rgba(16, 24, 40, 0.06), 0px 1px 3px 0px rgba(16, 24, 40, 0.10)',
@@ -116,59 +111,41 @@ export default {
         1.5: '1.5',
         2.5: '2.5',
       },
-      keyframes: ({ theme }) => ({
-        rerender: {
-          '0%': {
-            ['border-color']: theme('colors.vercel.pink'),
-          },
-          '40%': {
-            ['border-color']: theme('colors.vercel.pink'),
-          },
-        },
-        highlight: {
-          '0%': {
-            background: theme('colors.vercel.pink'),
-            color: theme('colors.white'),
-          },
-          '40%': {
-            background: theme('colors.vercel.pink'),
-            color: theme('colors.white'),
-          },
-        },
-        loading: {
-          '0%': {
-            opacity: '.2',
-          },
-          '20%': {
-            opacity: '1',
-            transform: 'translateX(1px)',
-          },
-          to: {
-            opacity: '.2',
-          },
-        },
-        shimmer: {
-          '100%': {
-            transform: 'translateX(100%)',
-          },
-        },
-        translateXReset: {
-          '100%': {
-            transform: 'translateX(0)',
-          },
-        },
-        fadeToTransparent: {
-          '0%': {
-            opacity: '1',
-          },
-          '40%': {
-            opacity: '1',
-          },
-          '100%': {
-            opacity: '0',
-          },
-        },
-      }),
+      // keyframes: ({ theme }) => ({
+      //   loading: {
+      //     '0%': {
+      //       opacity: '.2',
+      //     },
+      //     '20%': {
+      //       opacity: '1',
+      //       transform: 'translateX(1px)',
+      //     },
+      //     to: {
+      //       opacity: '.2',
+      //     },
+      //   },
+      //   shimmer: {
+      //     '100%': {
+      //       transform: 'translateX(100%)',
+      //     },
+      //   },
+      //   translateXReset: {
+      //     '100%': {
+      //       transform: 'translateX(0)',
+      //     },
+      //   },
+      //   fadeToTransparent: {
+      //     '0%': {
+      //       opacity: '1',
+      //     },
+      //     '40%': {
+      //       opacity: '1',
+      //     },
+      //     '100%': {
+      //       opacity: '0',
+      //     },
+      //   },
+      // }),
     },
   },
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
