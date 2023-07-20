d4c59e100b3ea209b7bd10ce79debfd82fd5d56b
Lucy Macartney <macartney.lucy@gmail.com>
2023-07-20 17:18:54 +0100

    removes default open

--- ui/components/Select/Select.tsx
+++ ui/components/Select/Select.tsx
@@ -10,7 +10,7 @@ export function Select({
   children: React.ReactNode;
 }) {
   return (
-    <SelectPrimitive.Root defaultOpen>
+    <SelectPrimitive.Root>
       <SelectPrimitive.Trigger className="border-input ring-offset-background data-[state=open]:border-brand-300 focus:border-brand-300 shadow-xs flex h-9 w-full items-center justify-between rounded-md border bg-transparent px-3 py-3 text-gray-600 placeholder:text-gray-600 focus:outline-none focus:ring-4 focus:ring-[#FFE3CC] disabled:cursor-not-allowed disabled:opacity-50 data-[state=open]:outline-none data-[state=open]:ring-4 data-[state=open]:ring-[#FFE3CC]">
         <SelectPrimitive.Value placeholder={placeholder} />
         <SelectPrimitive.Icon asChild>
