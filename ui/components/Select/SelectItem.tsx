22ce9789fb9fb1a3c1e1a07a68d0ca060e8c27d2
Lucy Macartney <macartney.lucy@gmail.com>
2023-07-24 16:10:46 +0100

    Adds focus styling and checkicon

--- ui/components/Select/SelectItem.tsx
+++ ui/components/Select/SelectItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as SelectPrimitive from '@radix-ui/react-select';
 import clsx from 'clsx';
+import { CheckIcon } from '@heroicons/react/24/outline';
 
 export const SelectItem = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Item>,
@@ -11,11 +12,15 @@ export const SelectItem = React.forwardRef<
     {...props}
     className={clsx(
       className,
-      'relative flex w-full cursor-default select-none items-center rounded-sm px-2 py-2.5 outline-none hover:bg-gray-50 data-[disabled]:pointer-events-none data-[state=checked]:bg-gray-50 data-[disabled]:opacity-50 dark:text-gray-200 dark:hover:bg-gray-800 dark:data-[state=checked]:bg-gray-800',
+      'relative flex w-full cursor-default select-none items-center rounded-sm px-2 py-2.5  outline-none focus:bg-gray-50 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:text-gray-200 dark:focus:bg-gray-800',
     )}
   >
     <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
-    <SelectPrimitive.ItemIndicator />
+    <span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
+      <SelectPrimitive.ItemIndicator>
+        <CheckIcon className="text-brand h-4 w-4" />
+      </SelectPrimitive.ItemIndicator>
+    </span>
   </SelectPrimitive.Item>
 ));
 SelectItem.displayName = SelectPrimitive.Item.displayName;
