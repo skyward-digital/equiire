da607efa30b317b1a6c35e226c0ab1d75aa33d0e
Lucy Macartney <macartney.lucy@gmail.com>
2023-07-26 14:33:27 +0100

    adds cursor to radiogroup

--- ui/components/RadioGroup/RadioGroup.tsx
+++ ui/components/RadioGroup/RadioGroup.tsx
@@ -36,13 +36,13 @@ export function RadioGroup({
         value={leftValue}
         id={`${id}-left`}
         className={clsx(
-          'flex items-center py-2 focus:outline-none ',
+          'flex items-center py-2 focus:outline-none',
           value === leftValue
             ? 'bg-brand focus:ring-brand-100 focus:border-brand-300 z-10 rounded-full px-7 text-white focus:ring-2 focus:ring-offset-2'
             : 'z-0 rounded-l-full bg-gray-100 pl-7 pr-14 text-gray-400 dark:bg-gray-700 dark:text-gray-500',
         )}
       >
-        <Label htmlFor={`${id}-left`} className="font-semibold">
+        <Label htmlFor={`${id}-left`} className="cursor-pointer font-semibold">
           {leftLabel}
         </Label>
       </RadioGroupPrimitive.Item>
@@ -56,7 +56,7 @@ export function RadioGroup({
             : 'z-0 rounded-r-full bg-gray-100 pl-14 pr-7 text-gray-400 dark:bg-gray-700 dark:text-gray-500',
         )}
       >
-        <Label htmlFor={`${id}-right`} className="font-semibold">
+        <Label htmlFor={`${id}-right`} className="cursor-pointer font-semibold">
           {rightLabel}
         </Label>
       </RadioGroupPrimitive.Item>
