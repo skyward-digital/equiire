6b9e09ace68c99da177b748f1a256d4070b34a04
Dan Spratling <dan@skyward.digital>
2023-07-27 16:43:37 +0100

    Adds user data to header

--- ui/components/Header/Header.tsx
+++ ui/components/Header/Header.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 import { Logo } from '#/ui/assets/Logo';
 import { DarkModeToggle } from './DarkModeToggle';
-import { UserProfile } from './UserProfile';
+import UserProfile from './UserProfile';
 
 export const Header = () => {
   return (
-    <header className="bg-brand border-brand fixed top-0 z-10 flex max-h-20 w-full items-center justify-between border-b px-6 py-2 dark:border-gray-600 dark:bg-black">
+    <header className="from-brand to-brand-400 fixed top-0 z-10 flex max-h-20 w-full items-center justify-between border-b border-transparent bg-gradient-to-r px-6 py-2 dark:border-gray-600 dark:bg-black">
       <div className="flex h-14 items-center px-4 py-4 lg:h-auto">
         <Link href="/" className="group flex w-full items-center gap-x-2.5">
           <Logo className="dark:text-brand fill-current text-white" />
--- ui/components/Header/UserProfile.tsx
+++ ui/components/Header/UserProfile.tsx
@@ -1,18 +1,36 @@
 import Link from 'next/link';
+import { getServerSession } from 'next-auth';
+import { authOptions, AuthSession } from '#/lib/auth';
 import { Avatar } from '#/ui/components/Avatar';
 
-export function UserProfile({ className }: { className: string }) {
+type UserProfileProps = {
+  user: AuthSession['user'];
+  className: string;
+};
+
+export function UserProfile({ user, className }: UserProfileProps) {
+  const username = user?.company ? user.company : user.name;
+  const avatar = username
+    .split(' ')
+    .map((n) => n[0])
+    .join('');
+
   return (
     <div className={className}>
       <Link
         href="/settings"
         className="flex items-center gap-2 rounded-lg p-2.5"
       >
-        <p>Dan Spratling</p>
-        <Avatar className="bg-orange-500" width={24} height={24}>
-          DS
+        <p>{user.company ? user.company : user.name}</p>
+        <Avatar className="bg-brand-500" width={24} height={24}>
+          {avatar}
         </Avatar>
       </Link>
     </div>
   );
+}
+
+export default async function UserProfileComposable(props: any) {
+  const session = (await getServerSession(authOptions)) as AuthSession;
+  return <UserProfile user={session.user} {...props} />;
 }
