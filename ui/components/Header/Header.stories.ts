b6ce3bd3bd3ebd81dbd9cca2b50c5783a0fec503
Dan Spratling <dan@skyward.digital>
2023-07-27 18:25:34 +0100

    Type fixes

--- ui/components/Header/Header.stories.ts
+++ ui/components/Header/Header.stories.ts
@@ -1,12 +1,10 @@
+import { AuthSession } from '#/lib/auth';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Header } from '.';
 
 const meta: Meta<typeof Header> = {
   title: 'Components/Header',
   component: Header,
-  argTypes: {
-    title: { control: 'text' },
-  },
   parameters: {
     docs: {
       description: {
@@ -28,6 +26,20 @@ export const Default: Story = {
       name: 'John Doe',
       email: 'johndoe@acme.com',
       company: 'Acme Inc',
+      sub: '123',
+      address: {
+        addressLine1: '123 Main St',
+        addressLine2: 'Suite 100',
+        city: 'Anytown',
+        state: 'CA',
+        postalCode: '12345',
+        country: 'USA',
+      },
+      phone: '123-456-7890',
+      contact: 'Jane Doe',
+      recordStatus: 'active',
+      paymentCustomerId: 'cus_123',
+      __v: 0,
     },
   },
 };
