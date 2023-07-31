3eb0c6204ff358ae2a61ea9917003c25fa4c3b3c
Lucy Macartney <64803272+lmac-1@users.noreply.github.com>
2023-07-31 09:42:01 +0100

    Adds select component for US state in sign up form (#41)

--- ui/components/SignupForm/AdditionalDetailsForm.tsx
+++ ui/components/SignupForm/AdditionalDetailsForm.tsx
@@ -1,6 +1,9 @@
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
+import statesFullList from 'states-us';
 import { Input } from '#/ui/components/Form/Input';
 import { Button } from '#/ui/components/Button';
+import { Select, SelectItem } from '#/ui/components/Select';
+import { Label } from '../Label';
 
 export function AdditionalDetailsForm({
   setStep,
@@ -15,12 +18,20 @@ export function AdditionalDetailsForm({
     register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm();
   const onSubmit = (data: any) => {
     console.log(data);
     setFormData({ ...formData, ...data });
     setStep((step: number) => step + 1);
   };
+
+  const states = statesFullList
+    .filter((state) => !state.territory)
+    .map((state) => ({
+      label: state.name,
+      value: state.abbreviation,
+    }));
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="grid max-w-2xl gap-6">
@@ -55,18 +66,28 @@ export function AdditionalDetailsForm({
           className="col-span-2 sm:col-span-2"
           autocomplete="address-level2"
         />
-        {/* Todo: change to select component  */}
-        <Input
-          id="state"
-          label="State"
-          register={register}
-          placeholder="Alabama"
-          required="State is required"
-          error={errors.select}
-          className="col-span-1 sm:col-span-2"
-          autocomplete="address-level1"
-        />
-        {/* Todo: add inputMode prop (currently in other PR) */}
+        <div className="col-span-2 mb-6 flex flex-col">
+          <Label
+            className="mb-2 font-semibold text-gray-600 dark:text-gray-400"
+            htmlFor="state"
+          >
+            State
+          </Label>
+          <Controller
+            name="state"
+            control={control}
+            defaultValue="AL"
+            render={({ field: { onChange, name, value } }) => (
+              <Select id={name} value={value} onValueChange={onChange}>
+                {states.map((state) => (
+                  <SelectItem key={state.value} value={state.value}>
+                    {state.label}
+                  </SelectItem>
+                ))}
+              </Select>
+            )}
+          />
+        </div>
         <Input
           id="zipcode"
           type="text"
@@ -75,8 +96,9 @@ export function AdditionalDetailsForm({
           register={register}
           required="Zip Code is required"
           error={errors.zipcode}
-          className="col-span-1 sm:col-span-1"
+          className="col-span-2 sm:col-span-1"
           autocomplete="postal-code"
+          inputMode="numeric"
         />
       </div>
       <Input
