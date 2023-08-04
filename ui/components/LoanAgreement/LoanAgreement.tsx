67e10ae84ec48f2357c4e26f689d826ac21a7725
Lucy Macartney <macartney.lucy@gmail.com>
2023-08-04 14:12:08 +0100

    might fix hydration error?

--- ui/components/LoanAgreement/LoanAgreement.tsx
+++ ui/components/LoanAgreement/LoanAgreement.tsx
@@ -30,9 +30,9 @@ export function LoanAgreement({ iframeUrl }: LoanAgreementProps) {
   };
 
   return (
-    <DialogPrimitive.Root onOpenChange={handleOpenChange}>
+    <DialogPrimitive.Root defaultOpen onOpenChange={handleOpenChange}>
       <DialogPrimitive.Trigger />
-      <DialogPrimitive.Portal>
+      <DialogPrimitive.Portal container={document.body}>
         <DialogPrimitive.Overlay className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/40 backdrop-blur-sm" />
         <DialogPrimitive.Content className="dark:border-brand-secondary data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] shadow-xs fixed left-[50%] top-[50%] z-50 grid h-full w-full max-w-4xl translate-x-[-50%] translate-y-[-50%] border bg-white duration-200 sm:h-full">
           <iframe src={iframeUrl} className="mt-10 h-full w-full sm:mt-0" />
