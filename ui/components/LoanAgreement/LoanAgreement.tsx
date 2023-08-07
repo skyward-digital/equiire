3e347792d72cd855bd2702fdf7265c8ee4b96241
Lucy Macartney <macartney.lucy@gmail.com>
2023-08-07 13:09:15 +0100

    fixes button

--- ui/components/LoanAgreement/LoanAgreement.tsx
+++ ui/components/LoanAgreement/LoanAgreement.tsx
@@ -8,19 +8,6 @@ type LoanAgreementProps = {
 };
 
 export function LoanAgreement({ iframeUrl }: LoanAgreementProps) {
-  // Stops hydration errors on parent server component
-  /* const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return <></>; */
-  /* const [isOpen, setIsOpen] = useState(false);
-  useEffect(() => {
-    setIsOpen(true);
-  }, []); */
-
   const handleOpenChange = (open: boolean) => {
     if (!open) {
       const currentUrl = new URL(window.location.href);
@@ -37,8 +24,8 @@ export function LoanAgreement({ iframeUrl }: LoanAgreementProps) {
         <DialogPrimitive.Content className="dark:border-brand-secondary data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] shadow-xs fixed left-[50%] top-[50%] z-50 grid h-full w-full max-w-4xl translate-x-[-50%] translate-y-[-50%] border bg-white duration-200 sm:h-full">
           <iframe src={iframeUrl} className="mt-10 h-full w-full sm:mt-0" />
 
-          <DialogPrimitive.Close className="ring-offset-brand focus:ring-ring absolute right-2 top-2 rounded-full bg-white transition-opacity focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-white sm:-right-10 sm:top-4">
-            <XMarkIcon className="h-5 w-5 " />
+          <DialogPrimitive.Close className="ring-offset-brand focus:ring-ring absolute right-4 top-4 rounded-full border border-gray-300 bg-white p-3 text-gray-600 transition-opacity focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-white dark:border-none dark:bg-black dark:text-gray-300 sm:-right-14 sm:top-4">
+            <XMarkIcon className="h-4 w-4" strokeWidth={2.5} />
           </DialogPrimitive.Close>
         </DialogPrimitive.Content>
       </DialogPrimitive.Portal>
