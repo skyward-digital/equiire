a0a99b8c63adff1b76fe22df8229ffc27f6f4239
Lucy Macartney <macartney.lucy@gmail.com>
2023-08-07 11:16:41 +0100

    moves state up to page

--- ui/components/SignupForm/SignupForm.tsx
+++ ui/components/SignupForm/SignupForm.tsx
@@ -1,13 +1,9 @@
-'use client';
-import { useState } from 'react';
 import { ProgressSteps } from '#/ui/components/ProgressSteps';
 import { LoginCard } from '#/ui/components/LoginCard';
 import { PersonalInformationForm } from './PersonalInformationForm';
 import { AdditionalDetailsForm } from './AdditionalDetailsForm';
 import { PasswordForm } from './PasswordForm';
-import { Loan } from '#/app/(login)/sign-up/page';
-import { OtpForm } from '#/ui/components/OtpForm';
-import { useRouter } from 'next/navigation';
+import { FormData, Loan } from '#/app/(login)/sign-up/page';
 
 const FORM_STEPS = [
   { title: 'Personal Information', component: PersonalInformationForm },
@@ -15,54 +11,24 @@ const FORM_STEPS = [
   { title: 'Password', component: PasswordForm },
 ];
 
-export interface FormData {
-  email: string;
-  addressLine1: string;
-  addressLine2?: string;
-  city: string;
-  company: string;
-  name: string;
-  postalCode: string;
-  state: string;
-}
+export type SignupFormProps = {
+  step: number;
+  setStep: React.Dispatch<React.SetStateAction<number>>;
+  formData: FormData;
+  loan: Loan;
+  setFormData: (formData: FormData) => void;
+  setFormSubmitted: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-export function SignupForm({ loan }: { loan: Loan }) {
-  const [step, setStep] = useState(0);
-  const [formData, setFormData] = useState<FormData>({
-    email: '',
-    addressLine1: '',
-    addressLine2: '',
-    city: '',
-    company: '',
-    name: '',
-    postalCode: '',
-    state: '',
-  });
-  const [formSubmitted, setFormSubmitted] = useState(false);
+export function SignupForm({
+  step,
+  setStep,
+  formData,
+  loan,
+  setFormData,
+  setFormSubmitted,
+}: SignupFormProps) {
   const { component: CurrentFormComponent, title } = FORM_STEPS[step];
-  const router = useRouter();
-
-  // What the user will see after submitting the sign up form (OTP form)
-  if (formSubmitted) {
-    return (
-      <LoginCard
-        title="Confirm your email address"
-        description="We have sent you an email. You can enter the code from the email below to confirm your email address."
-        className="sm:mt-20"
-        back={() => {
-          setStep(0);
-          setFormSubmitted(false);
-        }}
-      >
-        <OtpForm
-          onSuccess={(otp) =>
-            router.push(`/confirm-email?email=${formData.email}&code=${otp}`)
-          }
-        />
-      </LoginCard>
-    );
-  }
-
   return (
     <LoginCard
       title="Complete your loan application"
