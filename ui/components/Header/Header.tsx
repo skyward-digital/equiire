6e487b22e20ba64e6b679559639a485a1a440c02
Lucy Macartney <macartney.lucy@gmail.com>
2023-08-10 08:56:12 +0100

    makes header responsive

--- ui/components/Header/Header.tsx
+++ ui/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import { User } from '#/app/api/profile/user';
 
 export const Header = ({ user }: { user: User }) => {
   return (
-    <header className="from-brand to-brand-400 fixed top-0 z-10 flex max-h-20 w-full items-center justify-between border-b border-transparent bg-gradient-to-r px-6 py-2 dark:border-gray-600 dark:bg-black">
+    <header className="from-brand to-brand-400 fixed top-0 z-10 flex max-h-20 w-full items-center justify-center border-b border-transparent bg-gradient-to-r px-6 py-2 dark:border-gray-600 dark:bg-black lg:justify-between">
       <div className="flex h-14 items-center px-4 py-4 lg:h-auto">
         <Link href="/" className="group flex w-full items-center gap-x-2.5">
           <Logo className="dark:text-brand fill-current text-white" />
@@ -15,8 +15,8 @@ export const Header = ({ user }: { user: User }) => {
       </div>
 
       <div className="flex items-center gap-4 text-sm font-semibold text-white">
-        <DarkModeToggle />
-        <UserProfile user={user} className="hidden sm:block" />
+        <DarkModeToggle className="hidden lg:block" />
+        <UserProfile user={user} className="hidden lg:block" />
       </div>
     </header>
   );
