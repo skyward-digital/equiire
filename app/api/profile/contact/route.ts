d6c3b39494932c005ed3e136a68346435644bb1b
Dan Spratling <dan@skyward.digital>
2023-08-11 10:11:05 +0100

    Adds new APIs
    These are local APIs as all of these updates are happening client side

---
+++ app/api/profile/contact/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import { redirect } from 'next/navigation';
+import { getServerSession } from '#/app/api/session';
+import { User } from '#/app/api/profile/user';
+
+export async function PATCH(request: Request) {
+  const { accessToken } = await getServerSession();
+  const { contact } = await request.json();
+
+  const res = await fetch(
+    `${process.env.API_URL}/profile/contact?access_token=${accessToken}`,
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        contact,
+      }),
+    },
+  );
+
+  if (res.status === 401) redirect('/login');
+  if (!res.ok) throw new Error("Failed to update user's contact");
+
+  const user = (await res.json()) as { data: User };
+
+  return NextResponse.json({ data: user.data });
+}
