f97bfd3f3c89897d91b50038f10f16c4dd6ac382
Dan Spratling <dan@skyward.digital>
2023-08-16 09:56:43 +0100

    Removes contact api functionality

--- app/api/profile/contact/route.ts
+++ app/api/profile/contact/route.ts
@@ -1,29 +1,31 @@
-import { NextResponse } from 'next/server';
-import { redirect } from 'next/navigation';
-import { getServerSession } from '#/app/api/session';
-import { User } from '#/app/api/profile/user';
+// This file is for updating the delegated contact field, but as we're not using that right now it's commented out.
 
-export async function PATCH(request: Request) {
-  const { accessToken } = await getServerSession();
-  const { contact } = await request.json();
+// import { NextResponse } from 'next/server';
+// import { redirect } from 'next/navigation';
+// import { getServerSession } from '#/app/api/session';
+// import { User } from '#/app/api/profile/user';
 
-  const res = await fetch(
-    `${process.env.API_URL}/profile/contact?access_token=${accessToken}`,
-    {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        contact,
-      }),
-    },
-  );
+// export async function PATCH(request: Request) {
+//   const { accessToken } = await getServerSession();
+//   const { contact } = await request.json();
 
-  if (res.status === 401) redirect('/login');
-  if (!res.ok) throw new Error("Failed to update user's contact");
+//   const res = await fetch(
+//     `${process.env.API_URL}/profile/contact?access_token=${accessToken}`,
+//     {
+//       method: 'PATCH',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         contact,
+//       }),
+//     },
+//   );
 
-  const user = (await res.json()) as { data: User };
+//   if (res.status === 401) redirect('/login');
+//   if (!res.ok) throw new Error("Failed to update user's contact");
 
-  return NextResponse.json({ data: user.data });
-}
+//   const user = (await res.json()) as { data: User };
+
+//   return NextResponse.json({ data: user.data });
+// }
