08f47053b899e7ebe59a32d887e805d9b3bb7e43
Dan Spratling <dan@skyward.digital>
2023-08-16 10:03:15 +0100

    Lint fixes

--- app/api/profile/contact/route.ts
+++ app/api/profile/contact/route.ts
@@ -5,27 +5,23 @@
 // import { getServerSession } from '#/app/api/session';
 // import { User } from '#/app/api/profile/user';
 
-// export async function PATCH(request: Request) {
-//   const { accessToken } = await getServerSession();
-//   const { contact } = await request.json();
-
-//   const res = await fetch(
-//     `${process.env.API_URL}/profile/contact?access_token=${accessToken}`,
-//     {
-//       method: 'PATCH',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({
-//         contact,
-//       }),
-//     },
-//   );
-
-//   if (res.status === 401) redirect('/login');
-//   if (!res.ok) throw new Error("Failed to update user's contact");
-
-//   const user = (await res.json()) as { data: User };
-
-//   return NextResponse.json({ data: user.data });
-// }
+export async function PATCH(request: Request) {
+  //   const { accessToken } = await getServerSession();
+  //   const { contact } = await request.json();
+  //   const res = await fetch(
+  //     `${process.env.API_URL}/profile/contact?access_token=${accessToken}`,
+  //     {
+  //       method: 'PATCH',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({
+  //         contact,
+  //       }),
+  //     },
+  //   );
+  //   if (res.status === 401) redirect('/login');
+  //   if (!res.ok) throw new Error("Failed to update user's contact");
+  //   const user = (await res.json()) as { data: User };
+  //   return NextResponse.json({ data: user.data });
+}
