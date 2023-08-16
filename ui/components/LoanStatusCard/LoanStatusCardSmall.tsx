ca87fd0b5dca40db57405cb0996d54ea9d78b8f6
Lucy Macartney <64803272+lmac-1@users.noreply.github.com>
2023-08-16 09:50:44 -0500

    Mobile review of overview dashboard (home page) (#67)

--- ui/components/LoanStatusCard/LoanStatusCardSmall.tsx
+++ ui/components/LoanStatusCard/LoanStatusCardSmall.tsx
@@ -15,10 +15,10 @@ export const LoanStatusCardSmall = ({
 
   return (
     <div className="w-full rounded-xl border bg-white shadow-sm dark:border-gray-600 dark:bg-black">
-      <div className="flex w-full justify-between gap-4 border-b px-8 py-4 dark:border-gray-600">
-        <div className="flex items-center gap-4">
+      <div className="flex w-full flex-col justify-between gap-2 border-b px-4 py-4 dark:border-gray-600 sm:flex-row sm:gap-4 sm:px-8">
+        <div className="flex items-center justify-between gap-4">
           <Link href={`/loans/${id}`}>
-            <p className="font-brand -mb-1 text-xl font-semibold text-gray-400 dark:text-gray-200">
+            <p className="font-brand -mb-1 text-sm text-gray-400 dark:text-gray-200 sm:text-xl sm:font-semibold">
               #{id}
             </p>
           </Link>
@@ -37,7 +37,7 @@ export const LoanStatusCardSmall = ({
           <div className="flex items-center gap-4">
             <Link
               href={`/loans/${id}`}
-              className="hover:text-brand focus:text-brand font-semibold text-gray-600 duration-200"
+              className="hover:text-brand focus:text-brand ml-auto font-semibold text-gray-600 duration-200 sm:mx-0"
             >
               View
             </Link>
@@ -45,8 +45,8 @@ export const LoanStatusCardSmall = ({
         )}
       </div>
 
-      <div className="flex w-full flex-col p-8">
-        <h3 className="font-brand mb-1.5 text-3xl">
+      <div className="flex w-full flex-col p-4 sm:p-8">
+        <h3 className="font-brand mb-1.5 text-2xl sm:text-3xl">
           Loan of{' '}
           <strong className="text-brand">
             {value.toLocaleString('en-US', {
@@ -55,10 +55,11 @@ export const LoanStatusCardSmall = ({
               maximumFractionDigits: 0,
             })}
           </strong>{' '}
+          <br className="sm:hidden" />
           in <strong className="text-brand">{startDate}</strong>
         </h3>
         {status === 'completed' && endDate && (
-          <p className="font-brand text-gray-400 dark:text-gray-200">
+          <p className="font-brand sm:text-md text-sm text-gray-400 dark:text-gray-200">
             Final payment made on {endDate}
           </p>
         )}
