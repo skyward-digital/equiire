c909da2136d55a371eede178ac073360c3456d35
Dan Spratling <dan@skyward.digital>
2023-08-17 09:18:19 +0100

    Corrects baseurl

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,7 +1,7 @@
 import { cache } from 'react';
 
 export const getBaseUrl = cache(() => {
-  const envUrl = process.env.baseUrl || `https://${process.env.VERCEL_URL}`;
+  const envUrl = process.env.BASE_URL || `https://${process.env.VERCEL_URL}`;
 
   return envUrl ? envUrl : `http://localhost:${process.env.PORT ?? 3000}`;
 });
