4045628baec12e6b8e783b8adefd4688ac41d9a9
Lucy Macartney <64803272+lmac-1@users.noreply.github.com>
2023-08-22 03:38:54 -0500

    Eng 161 disable standard loan option on loan calculator (#87)

--- ui/components/RadioGroup/RadioGroup.tsx
+++ ui/components/RadioGroup/RadioGroup.tsx
@@ -5,6 +5,7 @@ import { Label } from '#/ui/components/Label';
 export type Option = {
   label: string;
   value: string;
+  disabled?: boolean;
 };
 
 export type RadioGroupProps = {
@@ -24,8 +25,16 @@ export function RadioGroup({
   id,
   className,
 }: RadioGroupProps) {
-  const { label: leftLabel, value: leftValue } = options[0];
-  const { label: rightLabel, value: rightValue } = options[1];
+  const {
+    label: leftLabel,
+    value: leftValue,
+    disabled: leftDisabled,
+  } = options[0];
+  const {
+    label: rightLabel,
+    value: rightValue,
+    disabled: rightDisabled,
+  } = options[1];
 
   return (
     <RadioGroupPrimitive.Root
@@ -38,25 +47,28 @@ export function RadioGroup({
         value={leftValue}
         id={`${id}-left`}
         className={clsx(
-          'flex items-center py-2 focus:outline-none',
+          'flex items-center py-2 focus:outline-none disabled:pointer-events-none',
           value === leftValue
             ? 'bg-brand focus:ring-brand-100 focus:border-brand-300 z-10 rounded-full px-7 text-white focus:ring-2 focus:ring-offset-2'
             : 'z-0 rounded-l-full bg-gray-100 pl-7 pr-14 text-gray-400 dark:bg-gray-700 dark:text-gray-500',
         )}
+        disabled={leftDisabled}
       >
         <Label htmlFor={`${id}-left`} className="cursor-pointer font-semibold">
           {leftLabel}
         </Label>
       </RadioGroupPrimitive.Item>
+
       <RadioGroupPrimitive.Item
         value={rightValue}
         id={`${id}-right`}
         className={clsx(
-          '-ml-7 flex items-center py-2 focus:outline-none ',
+          '-ml-7 flex items-center py-2 focus:outline-none disabled:pointer-events-none',
           value === rightValue
             ? 'bg-brand focus:ring-brand-100 focus:border-brand-300 z-10 rounded-full px-7 text-white focus:ring-2 focus:ring-offset-2'
             : 'z-0 rounded-r-full bg-gray-100 pl-14 pr-7 text-gray-400 dark:bg-gray-700 dark:text-gray-500',
         )}
+        disabled={rightDisabled}
       >
         <Label htmlFor={`${id}-right`} className="cursor-pointer font-semibold">
           {rightLabel}
