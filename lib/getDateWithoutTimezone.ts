237aa28837c25063c1aa61d94039396db3cfc53d
Lucy Macartney <macartney.lucy@gmail.com>
2023-08-24 14:20:04 -0500

    Adds more comments

--- lib/getDateWithoutTimezone.ts
+++ lib/getDateWithoutTimezone.ts
@@ -1,9 +1,17 @@
+/* 
+All dates in the database are saved in the following format: YYYY-DD-MMT00:00:00.000Z
+The getDateWithoutTimezone function creates a new Date object using only the year, month, and date to avoid timezone issues from UTC time.
+
+This function is only used on the frontend:
+- To display the correct date to the user
+- To evaluate any date display logic (for example, show the loans with a start date before today)
+    -> If you're making any comparisons, make sure to apply this function to all of the dates you are comparing
+*/
 export const getDateWithoutTimezone = (date: Date | string) => {
   if (typeof date === 'string') {
     date = new Date(date);
   }
 
-  // Dates in the db are saved in the following format: YYYY-DD-MMT00:00:00.000Z
-  // Creates a new Date object using only the year, month, and date to avoid timezone issues from UTC time.
+  // Creates a new Date object using only the year, month, and date
   return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
 };
